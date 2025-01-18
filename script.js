fe4d1113bafb6916d9cd64b1fd12fe91c410dd18
Macnelson9 <michaelofatu@gmail.com>
2025-01-18 14:51:04 +0100

    Added Features v3.0 For this commit, I have implemented the following features: - Logic to declare a winner after scoring 10 points. - Button and Logic to reset the game.

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ const buttons = document.querySelectorAll('button');
 const userScoreEl = document.getElementById('user-score');
 const computerScoreEl = document.getElementById('computer-score');
 const resultEl = document.getElementById('result');
+const winnerEl = document.getElementById('winner');
+const resetBtn = document.querySelector('.reset');
 
 let playerScore = 0;
 let computerScore = 0;
 
-buttons.forEach(button => {
+buttons.forEach(button =>
   button.addEventListener('click', function () {
     const result = playRound(button.id, computerPlay());
     resultEl.textContent = result;
-  });
-});
+
+    declareWinner(playerScore, computerScore);
+  })
+);
 
 const computerPlay = function () {
   const choices = ['rock', 'paper', 'scissors'];
@@ -24,11 +28,11 @@ const computerPlay = function () {
 
 const playRound = function (playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "It's a tie!";
+    return `It's a tie`;
   } else if (
     (playerSelection === 'rock' && computerSelection === 'scissors') ||
-    (playerSelection === 'paper' && computerSelection === 'rock') ||
-    (playerSelection === 'scissors' && computerSelection === 'paper')
+    (playerSelection === 'scissors' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'rock')
   ) {
     playerScore++;
     userScoreEl.textContent = playerScore;
@@ -37,6 +41,26 @@ const playRound = function (playerSelection, computerSelection) {
   } else {
     computerScore++;
     computerScoreEl.textContent = computerScore;
+
     return `You lose! ${computerSelection} beats ${playerSelection}`;
   }
 };
+
+const declareWinner = function (playerScore, computerScore) {
+  if (playerScore >= 10 && computerScore < 10) {
+    winnerEl.textContent = `Congratulations!! You win!!! 🏆🏆`;
+  } else if (playerScore === 10 && computerScore === 10) {
+    winnerEl.textContent = `It ends in a Tie! Play Again?? 🎮`;
+  } else if (playerScore < 10 && computerScore >= 10) {
+    winnerEl.textContent = `Sorry!! You lose!! Play Again?? 😝🕹`;
+  }
+};
+
+resetBtn.addEventListener('click', function () {
+  playerScore = 0;
+  computerScore = 0;
+  resultEl.textContent = '';
+  userScoreEl.textContent = '0';
+  computerScoreEl.textContent = '0';
+  winnerEl.textContent = '';
+});
